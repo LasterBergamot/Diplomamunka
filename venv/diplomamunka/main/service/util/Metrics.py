df84ebc20860c803d511bfc466dcc10dc06b4427
Andras Torok <torokandras1996@gmail.com>
2020-02-08 17:59:11 +0100

    Added the signature of metric methods.

--- venv/diplomamunka/main/service/util/Metrics.py
+++ venv/diplomamunka/main/service/util/Metrics.py
@@ -2,3 +2,22 @@ class Metrics:
 
     def showMetrics(self):
         print("Will show metrics here...")
+
+    def MAE(self):
+        print("Will calculate Mean Absolute Error here...")
+
+    def RMSE(self):
+        print("Will calculate Root Mean Squared Error here...")
+
+    def coverage(self):
+        print("Will calculate coverage here...")
+
+    def diversity(self):
+        print("Will calculate diversity here...")
+
+    def novelty(self):
+        print("Will calculate novelty here...")
+
+    # Scalability = time requirement of evaluation
+    def scalability(self):
+        print("Will calculate scalability here...")
